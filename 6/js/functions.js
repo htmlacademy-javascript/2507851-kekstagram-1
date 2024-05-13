859e668ccdf4d92fddd6b96579dfdfa8daa4dbfb
Keks <keks@htmlacademy.ru>
2024-05-13 11:10:33 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/functions.js
@@ -0,0 +1,62 @@
+const isPalindrome = (str) => {
+  const tempStr = str.toLowerCase().replaceAll(' ', '');
+
+  for (let i = 0, j = tempStr.length - 1; i < j; i++, j--) {
+    if (tempStr[i] !== tempStr[j]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+isPalindrome('топот');
+// isPalindrome('ДовОд');
+// isPalindrome('Кекс');
+// isPalindrome('Лёша на полке клопа нашёл ');
+
+
+const convertToNumber = (str) => {
+  if (typeof str === 'number') {
+    return str;
+  }
+
+  let result = '';
+
+  for (const value of str) {
+    if (!Number.isNaN(parseInt(value,10))) {
+      result += value;
+    }
+  }
+
+  return parseInt(result, 10);
+};
+convertToNumber('2023 год');
+// имяФункции('2023 год');// 2023
+// имяФункции('ECMAScript 2022');// 2022
+// имяФункции('1 кефир, 0.5 батона'); // 105
+// имяФункции('агент 007');// 7
+// имяФункции('а я томат');
+
+
+const checkingStringLength = (str, length) => str.length <= length;
+
+checkingStringLength('проверяемая строка', 20);
+// checkingStringLength('проверяемая строка', 18);
+// checkingStringLength('проверяемая строка', 10);
+
+const myPadStart = (str, minLength, pad) => {
+  const actualPad = minLength - str.length;
+
+  if (actualPad <= 0) {
+    return str;
+  }
+
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + str;
+};
+
+myPadStart('1', 2, '0');
+// myPadStart('1', 4, '0');
+// myPadStart('q', 4, 'werty');
+// myPadStart('q', 4, 'we');
+// myPadStart('qwerty', 4, '0');
---
+++ 6/js/utils.js
@@ -0,0 +1,8 @@
+export const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+};
+
+export const getRandomArrayElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
+
