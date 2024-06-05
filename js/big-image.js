cd964b0c00c1fc87f16b6131cb5517368e64a02b
Alina <alianina2020@yandex.ru>
2024-06-05 21:04:38 +1100

    corrected edits

--- js/big-image.js
+++ js/big-image.js
@@ -1,11 +1,5 @@
 import {isEscapeKey} from './utils.js';
 
-const bigPicture = document.querySelector('.big-picture');
-const closeButton = bigPicture.querySelector('.big-picture__cancel');
-const commentsLoader = bigPicture.querySelector('.comments-loader');
-const commentsContainer = bigPicture.querySelector('.social__comments');
-const commentsToShowCount = bigPicture.querySelector('.social__comment-count');
-
 const onPopupEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
@@ -18,21 +12,27 @@ const onPopupCloseButtonClick = () => {
 };
 
 function closeBigPicture() {
+  const bigPicture = document.querySelector('.big-picture');
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
 }
 
 export const showBigPicture = (picture) => {
+  const bigPicture = document.querySelector('.big-picture');
+  const closeButton = bigPicture.querySelector('.big-picture__cancel');
+  const commentsLoader = bigPicture.querySelector('.comments-loader');
+  const commentsContainer = bigPicture.querySelector('.social__comments');
+  const commentsToShowCount = bigPicture.querySelector('.social__comment-count');
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
   commentsLoader.classList.add('hidden');
   commentsToShowCount.classList.add('hidden');
   commentsContainer.classList.add('hidden');
 
-  bigPicture.querySelector('.big-picture__img img').src = picture.url;
+  bigPicture.querySelector('.big-picture__img img').src = picture.src;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
-  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
+  bigPicture.querySelector('.comments-count').textContent = picture.comments;
   bigPicture.querySelector('.social__caption').textContent = picture.description;
 
   closeButton.addEventListener('click', onPopupCloseButtonClick);
--- js/gallery.js
+++ js/gallery.js
@@ -1,15 +1,16 @@
-import {showBigPicture} from './big-image.js';
+import { showBigPicture } from './big-image.js';
 
 const gallery = document.querySelector('#picture').content.querySelector('.picture');
 const containetTemplate = document.querySelector('.pictures');
 
-const createPhoto = ({ comments, description, likes, url}) => {
+const createPhoto = ({ comments, description, likes, url, id }) => {
   const galleryImage = gallery.cloneNode(true);
 
   galleryImage.querySelector('.picture__img').src = url;
   galleryImage.querySelector('.picture__img').alt = description;
   galleryImage.querySelector('.picture__comments').textContent = comments.length;
   galleryImage.querySelector('.picture__likes').textContent = likes;
+  galleryImage.dataset.galleryImageId = id;
 
   return galleryImage;
 };
@@ -20,10 +21,12 @@ export const renderGallery = (pictures) => {
   pictures.forEach((picture) => {
     const pictureGallery = createPhoto(picture);
     fragment.append(pictureGallery);
-    pictureGallery.addEventListener('click', () => {
-      showBigPicture(picture);
-    });
   });
-
   containetTemplate.append(fragment);
 };
+
+const pictureContainer = document.querySelector('.pictures');
+pictureContainer.addEventListener('click', (evt) => {
+  showBigPicture(evt.target);
+});
+
