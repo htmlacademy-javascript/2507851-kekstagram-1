36f58d66a597fdd7b2d39acddeb9163beafa1ade
Keks <keks@htmlacademy.ru>
2024-06-06 17:59:27 +0300

    :heavy_check_mark: Сборка #15

--- 15/js/main.js
+++ 15/js/main.js
@@ -1,5 +1,7 @@
+
 import { createPicturesList } from './data.js';
 import { renderGallery } from './gallery.js';
+// import { renderBigPictureTemplate } from './gallery.js';
 
 renderGallery(createPicturesList(25));
 
