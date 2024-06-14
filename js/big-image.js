0507f051ec33b319027be20a36afc5cb901699a0
Alina <alianina2020@yandex.ru>
2024-06-15 02:28:18 +1100

    вторая часть задания

--- js/big-image.js
+++ js/big-image.js
@@ -1,18 +1,51 @@
 import {isEscapeKey} from './utils.js';
 
+const PART_OF_THE_COMMENTS = 5;
+
 const bigPicture = document.querySelector('.big-picture');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const commentsContainer = bigPicture.querySelector('.social__comments');
 const commentsToShowCount = bigPicture.querySelector('.social__comment-count');
 
-function hideComments() {
-  commentsLoader.classList.add('hidden');
-  commentsToShowCount.classList.add('hidden');
-  commentsContainer.classList.add('hidden');
-}
+let commentsShown = 0;
+const comments = [];
 
-hideComments();
+const createComment = ({ avatar, name, message }) => {
+  const comment = document.createElement('li');
+  comment.innerHTML =
+  '<img class="social__picture" src="" alt="" width="35" height="35">';
+  comment.classList.add('social__comment');
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').src = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+const renderComments = () => {
+  commentsShown += PART_OF_THE_COMMENTS;
+
+  if (commentsShown >= comments.length) {
+    commentsLoader.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShown; i++) {
+    const commentElement = createComment(comments[i]);
+    fragment.append(commentElement);
+  }
+
+  commentsContainer.innerHTML = '';
+  commentsContainer.append(fragment);
+  commentsToShowCount.innerHTML = `${commentsShown} из <span class="comments-count">${comments.length}</span> комментариев`;
+};
+
+const commentsLoaderOnClick = () => renderComments();
 
 const onPopupEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -30,6 +63,7 @@ function closeBigPicture() {
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
   closeButton.removeEventListener('click', onPopupCloseButtonClick);
+  commentsLoader.removeEventListener('click', commentsLoaderOnClick);
 }
 
 export const showBigPicture = (picture) => {
@@ -43,4 +77,7 @@ export const showBigPicture = (picture) => {
 
   closeButton.addEventListener('click', onPopupCloseButtonClick);
   document.addEventListener('keydown', onPopupEscKeydown);
+  commentsLoader.addEventListener('click', commentsLoaderOnClick);
 };
+
+
