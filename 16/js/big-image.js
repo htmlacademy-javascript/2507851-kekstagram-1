dc4731ebde4fd957dcd1868dd4c7bfad4ab93398
Keks <keks@htmlacademy.ru>
2024-06-18 11:56:16 +0300

    :heavy_check_mark: Сборка #16

--- 16/js/big-image.js
+++ 16/js/big-image.js
@@ -1,6 +1,8 @@
 import {isEscapeKey} from './utils.js';
 
-const PART_OF_THE_COMMENTS = 5;
+const commentsListElement = document.querySelector('#comment').content.querySelector('.social__comment');
+
+const COMMENTS_PART = 5;
 
 const bigPicture = document.querySelector('.big-picture');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
@@ -9,44 +11,53 @@ const commentsContainer = bigPicture.querySelector('.social__comments');
 const commentsToShowCount = bigPicture.querySelector('.social__comment-count');
 
 let commentsShown = 0;
-
-const comments = [];
+let currentRenderCommentsHandler;
 
 const createComment = ({ avatar, name, message }) => {
-  const comment = document.createElement('li');
-  comment.innerHTML =
-  '<img class="social__picture" src="" alt="" width="35" height="35">';
-  comment.classList.add('social__comment');
+  const galleryComments = commentsListElement.cloneNode(true);
 
-  comment.querySelector('.social__picture').src = avatar;
-  comment.querySelector('.social__picture').src = name;
-  comment.querySelector('.social__text').textContent = message;
+  galleryComments.querySelector('.social__picture').src = avatar;
+  galleryComments.querySelector('.social__picture').alt = name;
+  galleryComments.querySelector('.social__text').textContent = message;
 
-  return comment;
+  return galleryComments;
 };
 
-const renderComments = () => {
-  commentsShown += PART_OF_THE_COMMENTS;
+const renderComments = (comments) => {
+  commentsShown += COMMENTS_PART;
 
   if (commentsShown >= comments.length) {
     commentsLoader.classList.add('hidden');
     commentsShown = comments.length;
   } else {
     commentsLoader.classList.remove('hidden');
+    commentsToShowCount.classList.remove('hidden');
   }
 
   const fragment = document.createDocumentFragment();
+
   for (let i = 0; i < commentsShown; i++) {
+
     const commentElement = createComment(comments[i]);
     fragment.append(commentElement);
   }
-
-  commentsContainer.innerHTML = '';
   commentsContainer.append(fragment);
-  commentsToShowCount.innerHTML = `${commentsShown} из <span class="comments-count">${comments.length}</span> комментариев`;
+  commentsToShowCount.querySelector('.show-comments-count').textContent = commentsShown;
 };
 
-const commentsLoaderOnClick = () => renderComments();
+const clickCommentsLoader = () => renderComments();
+
+const createRenderCommentsHandler = (comments) => {
+  renderComments(comments);
+};
+
+const renderDataComments = (commentElement) => {
+  commentsShown = 0;
+
+  if (commentElement.length > 0) {
+    renderComments(commentElement);
+  }
+};
 
 const onPopupEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -63,8 +74,17 @@ function closeBigPicture() {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
-  closeButton.removeEventListener('click', onPopupCloseButtonClick);
-  commentsLoader.removeEventListener('click', commentsLoaderOnClick);
+
+  if (currentRenderCommentsHandler) {
+    closeButton.removeEventListener('click', currentRenderCommentsHandler);
+    commentsLoader.removeEventListener('click', currentRenderCommentsHandler);
+    commentsToShowCount.removeEventListener('click', currentRenderCommentsHandler);
+  }
+  const resetComments = () => {
+    commentsShown = 0;
+    commentsContainer.innerHTML = '';
+  };
+  resetComments();
 }
 
 export const showBigPicture = (picture) => {
@@ -73,12 +93,15 @@ export const showBigPicture = (picture) => {
 
   bigPicture.querySelector('.big-picture__img img').src = picture.url;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
-  bigPicture.querySelector('.comments-count').textContent = picture.comments;
+  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
   bigPicture.querySelector('.social__caption').textContent = picture.description;
+
+  renderDataComments(picture.comments);
 
   closeButton.addEventListener('click', onPopupCloseButtonClick);
   document.addEventListener('keydown', onPopupEscKeydown);
-  commentsLoader.addEventListener('click', commentsLoaderOnClick);
+  currentRenderCommentsHandler = createRenderCommentsHandler(picture.comments);
+  commentsLoader.addEventListener('click', clickCommentsLoader);
+  currentRenderCommentsHandler = createRenderCommentsHandler(picture.comments);
+  commentsLoader.addEventListener('click', currentRenderCommentsHandler);
 };
-
-
