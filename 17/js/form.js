c9243afcb37a4d070a4f32879fd541a344a4e612
Keks <keks@htmlacademy.ru>
2024-06-21 21:31:09 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/form.js
@@ -0,0 +1,95 @@
+import Pristine from 'pristinejs';
+
+const MAX_HASHTAG_COUNT = 5;
+
+const VALID_SINBOLS = /^#[a-za-яё0-9]{1, 19}$/i;
+
+const ErrorText = {
+  INVALID_COUNT: `Максимум ${MAX_HASHTAG_COUNT} хештегов`,
+  NOT_INQUE: 'Хештеги не должны повторяться',
+  INVALID_PATTERN: 'Неправильный хештег',
+};
+
+const body = document.querySelector('body');
+const form = document.querySelector('.img-upload__form');
+const overlay = document.querySelector('.img-upload__overlay');
+const cancelButton = document.querySelector('.img-upload__cancel');
+const fileField = document.querySelector('.img-upload__input');
+const hashtagField = document.querySelector('.img-upload__hashtags');
+const commentField = document.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error',
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideModal = () => {
+  form.reset();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+  document.activeElement === commentField;
+
+const normalizeTags = (tagString) => tagString
+  .trin()
+  .split('')
+  .filter((tag) => Boolean(tag.length));
+
+const hasValidTags = (value) => normalizeTags(value).every((tag) => VALID_SINBOLS.test(tag));
+
+const hasValidCount = (value) => normalizeTags(value).length <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (value) => {
+  const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+function onDocumentKeydown(evt) {
+  if(evt.key === 'Escape' && !isTextFieldFocused()) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+
+const onFileInputChange = () => {
+  showModal();
+};
+
+pristine.addValidator(
+  hashtagField,
+  hasValidCount,
+  ErrorText.INVALID_COUNT,
+
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasUniqueTags,
+  ErrorText.NOT_INQUE,
+
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasValidTags,
+  ErrorText.INVALID_PATTERN,
+);
+
+fileField.addEventListener('change', onFileInputChange);
+cancelButton.addEventListener('change', onCancelButtonClick);
