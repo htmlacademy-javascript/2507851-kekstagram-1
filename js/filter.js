22563be6e651f29f1a83b7a12434eec7c4fb0959
Alina <alianina2020@yandex.ru>
2024-07-24 23:29:11 +1100

    переделала функции

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,4 @@
-import { debounce, shufflePhotos} from './utils.js';
+import { debounce, shufflePhotos } from './utils.js';
 import { initGallery } from './gallery.js';
 
 const RANDOM_PHOTO_COUNT = 10;
@@ -12,28 +12,36 @@ const FilterType = {
 
 const filtersContainer = document.querySelector('.img-filters');
 const filtersForm = filtersContainer.querySelector('.img-filters__form');
-const filterBtnDefault = filtersForm.querySelector('#filter-default');
-const filterBtnRandom = filtersForm.querySelector('#filter-random');
-const filterBtnDiscussed = filtersForm.querySelector('#filter-discussed');
 
-const getFilteredPhotos = {
-  [FilterType.DEFAULT]: (photos) => photos,
-  [FilterType.RANDOM]: (photos) => shufflePhotos(photos).slice(0, RANDOM_PHOTO_COUNT),
-  [FilterType.DISCUSSED]: (photos) => photos.slice().sort((a, b) => b.comments.length - a.comments.length)
+const sortByRandom = (photos) => shufflePhotos(photos).slice(0, RANDOM_PHOTO_COUNT);
+const sortByComment = (photos) =>photos.toSorted((a, b) => b.comments.length - a.comments.length);
+
+const getFilteredPhotos = (filter, photos) => {
+  switch (filter) {
+    case FilterType.RANDOM:
+      return sortByRandom(photos);
+    case FilterType.DISCUSSED:
+      return sortByComment(photos);
+    default:
+      return photos;
+  }
 };
 
 let currentFilter = FilterType.DEFAULT;
 
 const activateFilterButton = (element) => {
   const buttonActive = filtersContainer.querySelector('.img-filters__button--active');
-  buttonActive.classList.remove('img-filters__button--active');
-  element.target.classList.add('img-filters__button--active');
+
+  if (buttonActive) {
+    buttonActive.classList.remove('img-filters__button--active');
+  }
+  element.classList.add('img-filters__button--active');
 };
 
-const repaint = (element, filter, photos) => {
+const repaint = (filter, photos, element) => {
 
   if (currentFilter !== filter) {
-    const filteredPhotos = getFilteredPhotos[filter](photos);
+    const filteredPhotos = getFilteredPhotos(filter, photos);
     const photosContainer = document.querySelectorAll('.picture');
     photosContainer.forEach((photo) => photo.remove());
 
@@ -43,46 +51,16 @@ const repaint = (element, filter, photos) => {
   }
 };
 
-const debouncedRepaint = debounce(repaint, RERENDER_DELAY);
+const debouncedRepaint = debounce((filter, photos, element) => repaint(filter, photos, element), RERENDER_DELAY);
 
 export const initFilters = (photos) => {
   filtersContainer.classList.remove('img-filters--inactive');
 
-  filterBtnDefault.addEventListener('click', (evt) => {
-    debouncedRepaint(evt, FilterType.DEFAULT, photos);
-    activateFilterButton(evt);
-  });
+  filtersForm.addEventListener('click', (evt) => {
 
-  filterBtnRandom.addEventListener('click', (evt) => {
-    debouncedRepaint(evt, FilterType.RANDOM, photos);
-    activateFilterButton(evt);
-  });
-
-  filterBtnDiscussed.addEventListener('click', (evt) => {
-    debouncedRepaint(evt, FilterType.DISCUSSED, photos);
-    activateFilterButton(evt);
+    if (evt.target.classList.contains('img-filters__button')) {
+      const filter = evt.target.id.replace('filter-', '');
+      debouncedRepaint(filter, photos, evt.target);
+    }
   });
 };
-
-// const sortByRandom = (photos) => shufflePhotos(photos).slice(0, RANDOM_PHOTO_COUNT);
-
-// const sortByComment = (photos) =>photos.toSorted((a, b) => b.comments.length - a.comments.length);
-
-
-// export const initFilters(debounce(
-//   (clickedElement) => {
-
-//     switch (clickedElement.id) {
-//       case FilterType.DEFAULT:
-//         initGallery(pictures);
-//         break;
-//       case FilterType.RANDOM:
-//         initGallery(sortByRandom(pictures, RANDOM_PHOTO_COUNT));
-//         break;
-//       case FilterType.DISCUSSED:
-//         initGallery(sortByComment(pictures));
-//         break;
-//     }
-//   }, RERENDER_DELAY)
-// );
-
